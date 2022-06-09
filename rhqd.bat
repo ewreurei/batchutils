0e3a5a256364badb79def941e3dd1cb6d99e0b1d
ewre <97365039+ewreurei@users.noreply.github.com>
2022-06-10 05:10:11 +0900

    rhqd: Some better comments

--- rhqd.bat
+++ rhqd.bat
@@ -1,12 +1,13 @@
 @echo off
 setlocal
-rem require: rhq, tr(sd), cut, any interactive filter tool(like fzf, peco, etc. default is Heatseeker)
+rem require: rhq, tr(sd), cut, any interactive filter tool(like fzf, peco, etc)
 rem like unix shell image, cd $(rhq list | tr / \ | hs)
 
 rem Example: [C:\Users\%USERNAME%\rhq\]github.com\user\repo
 set rhqroot=%USERPROFILE%\rhq\
 rem Example:  C:\Users\%USERNAME%\rhq\[github.com\user\repo]
 set "cutfield=5-"
+rem set selcmd=
 
 for /f "usebackq delims=" %%i in (`rhq list ^| cut -d/ -f %cutfield% ^| tr / \ ^| %SELCMD% %*`) do ( set "dir=%%i" )
 endlocal & if not "%dir%"=="" ( pushd %rhqroot%%dir% )
