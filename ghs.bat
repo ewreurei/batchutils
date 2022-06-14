ed8ebeef0f1e1d9946a4a49acebbbf71c89be846
ewre <97365039+ewreurei@users.noreply.github.com>
2022-06-15 02:58:46 +0900

    ghs: Add curl's option

--- ghs.bat
+++ ghs.bat
@@ -4,5 +4,5 @@ setlocal
 if "%~1"=="" ( echo Usage: %~n0 ^<string^> & exit /b )
 call toenv query="url %*"
 rem curl "https://api.github.com/search/repositories?q=%query%" | xq -r ".items[].full_name"
-curl "https://api.github.com/search/repositories?q=%query%" ^
+curl -sSf "https://api.github.com/search/repositories?q=%query%" ^
 	| xq -r ".items[] | .full_name, .description, []" | sd "^\[\]" "" | rg --color=always -e "^[^ ]*/[^ ]*" -e "^"
