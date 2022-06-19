a13058ea8430e297402d2079c4d77f30051134c8
ewre <97365039+ewreurei@users.noreply.github.com>
2022-06-20 02:24:11 +0900

    tet: More simply code

--- tet.bat
+++ tet.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal enabledelayedexpansion
+setlocal
 rem tet - Minimum template expander (just a replacer)
 rem require: sd. and this batchfile uses temp file
 rem Example Usage:
@@ -9,23 +9,15 @@ rem   cat template.txt | tet name=John thing="vim settings"
 set "tmpfile=%TEMP%\tet-%random%.tmp"
 if "%~1"=="" ( echo Usage: %~n0 ^<variable^>=^<string^> & exit /b )
 
-call :catchargs %*
-
 sd -s -- "{{ %~1 }}" "%~2" > "%tmpfile%"
-for /l %%i in (2,1,%num%) do (
-	rem in place modification is sd's default
-	sd -s -- "{{ !name[%%i]! }}" "!value[%%i]!" "%tmpfile%"
-)
+call :loop %*
 
 type "%tmpfile%" & del "%tmpfile%"
 exit /b
 
-:catchargs
-	set "num=0"
-	:loop
-		set /a num+=1
-		set "name[%num%]=%~1"
-		set "value[%num%]=%~2"
-		shift & shift
-		if not "%~2"=="" ( goto :loop )
-	exit /b
+:loop
+	shift & shift
+	if "%~2"=="" ( exit /b )
+	rem in place modification is sd's default
+	sd -s -- "{{ %~1 }}" "%~2" "%tmpfile%"
+	goto :loop
