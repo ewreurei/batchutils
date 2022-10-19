93640804d6ce1c0d8c65e8b6559f102e989fc0eb
ewre <97365039+ewreurei@users.noreply.github.com>
2022-10-20 03:49:48 +0900

    ghrel: Improve error message
    as you can see, `<string>` is not enough

--- ghrel.bat
+++ ghrel.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 rem require: xq (jq), teip, Heatseeker (hs)
-if "%~1"=="" ( echo Usage: %~n0 ^<string^> & exit /b )
+if "%~1"=="" ( echo Usage: %~n0 ^<user^>/^<repo^> & exit /b )
 rem  | rg "msvc|windows|amd|exe"
 curl -sSf "https://api.github.com/repos/%~1/releases" | xq -r ".[0].assets[].browser_download_url" | teip -d/ -f 9 -- hs -s %~2 2>&1 | sd "^.* .*\n" "" | head -1
